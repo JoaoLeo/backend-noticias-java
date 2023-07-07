4dc4ba026e716bf20e81b74be17d4e539cdf0eb2
João Leonardo <joaoleonardo9921@gmail.com>
2023-07-07 14:44:55 -0300

    initial commit

---
+++ src/main/java/io/github/joaoleo/backendnoticiasjava/BackendNoticiasJavaApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaoleo.backendnoticiasjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNoticiasJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNoticiasJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/joaoleo/backendnoticiasjava/BackendNoticiasJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaoleo.backendnoticiasjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNoticiasJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
